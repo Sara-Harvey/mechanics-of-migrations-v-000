80273b01808aabedf9a72a60cbc92c033c1fc201
Sara Harvey <saraharveynyc@gmail.com>
2019-07-18 18:22:39 +0000

    Automatically backed up by Learn

--- config/environment.rb
+++ config/environment.rb
@@ -17,3 +17,8 @@ ActiveRecord::Base.establish_connection(
 )
  
 require_relative "../artist.rb"
+
+ActiveRecord::Base.establish_connection(
+  :adapter => "sqlite3",
+  :database => "db/artists.sqlite"
+)
