614182fa32d84a00433e7a443a675854c31fdc18
Sara Harvey <saraharveynyc@gmail.com>
2019-07-18 18:27:57 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_artists.rb
+++ db/migrate/01_create_artists.rb
@@ -1,17 +1,3 @@
-# db/migrate/01_create_artists.rb
- 
-class CreateArtists < ActiveRecord::Migration[5.2]
-  def up
-  end
- 
-  def down
-  end
-
-  def change
-    create_table :artists do |t|
-    end
-end
-
 class CreateArtists < ActiveRecord::Migration[4.2]
   def change
     create_table :artists do |t|
@@ -22,3 +8,4 @@ class CreateArtists < ActiveRecord::Migration[4.2]
     end
   end
 end
+
